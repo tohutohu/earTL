ab3d282b444c4dae74f7f37adf427f9eb458ca0b
to-hutohu <tohu.soy@gmail.com>
2017-05-27 13:05:08 +0900

    initial commit

---
+++ earTL.js
@@ -0,0 +1,45 @@
+const option = require('./token.js');
+const twit = require('twit');
+const client = new twit(option.twitter);
+var exec = require('child_process').exec;
+let muteList = [];
+let que = [];
+let playNow = false;
+exec('/mnt/c/Users/ok/Desktop/stn019320/softalk/SofTalk.exe /close');
+
+const stream = client.stream('user');
+client.get('mutes/users/list',{skip_status:true,include_entities:false},function(err,mutes,res){
+  muteList = mutes.users.map(user => user.screen_name);
+});
+
+stream.on('tweet',async tweet => {
+  if(muteList.indexOf(tweet.user.screen_name) !== -1){
+    console.log('mute');
+    return;
+  }
+  let text = tweet.user.name.replace(/@.+|\(|\)/g, '') + '    ' +tweet.text.replace(/\(|\)/g, '').replace(/\n/g, ' ');
+  text = text.replace(/https:\/\/t.co\/.+/, 'リンク ');
+  text = text.replace(/RT @(.+?)\s/, 'RT  ');
+  if(tweet.in_reply_to_screen_name != null){
+    const user = await client.get('users/lookup', {screen_name: tweet.in_reply_to_screen_name});
+    console.log(user);
+    text = text.replace(/@(.+?)\s/, '@' + user.data[0].name);
+  }
+  console.log(text);
+
+  que.push(text);
+  playTweet();
+});
+
+const playTweet = () => {
+  //if(playNow)return;
+  let text = que.shift();
+  const speed = 120;//Math.min(300, 100 + 10 * que.length + text.length);
+  playNow = true;
+  exec('/mnt/c/Users/ok/Desktop/stn019320/softalk/SofTalk.exe /X:1 /S:' + speed +' /W:' + text, () => {
+    playNow = false;
+  });
+  if(que.length > 0){
+    playTweet();
+  }
+}
